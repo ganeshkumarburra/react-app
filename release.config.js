b3eba089330fa4c14e8827185ab1890b1c40fa9f
GKR😍💚 <ganeshkumarburra2@gmail.com>
2022-09-15 23:25:00 +0530

    fix: added

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branchs: "master",
+  repositoryUrl: "https://github.com/ganeshkumarburra/react-app",
+  pulgins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
